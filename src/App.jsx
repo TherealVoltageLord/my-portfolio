fb53560d33431bc4c0bd3c343881789b041c51e4
VOLTAGE_LORD <officialvoltinstasnap@gmail.com>
2025-03-22 05:49:09 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,12 @@
+import React from "react";
 
+function App() {
+    return (
+        <div className="container text-center mt-5">
+            <h1>Welcome to Voltage Lord Portfolio</h1>
+            <p>Building APIs & Frontend solutions since 2020.</p>
+        </div>
+    );
+}
+
+export default App;

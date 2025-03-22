84c19ad9b03288bd21420276ea65ebfea23982b8
VOLTAGE_LORD <officialvoltinstasnap@gmail.com>
2025-03-22 06:57:31 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,13 @@
+import { Link } from "react-router-dom";
 
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Welcome to Voltage Lord's Portfolio</h1>
+      <p>Building the future with APIs and Frontend Magic!</p>
+      <Link to="/projects" className="btn">View Projects</Link>
+    </div>
+  );
+};
+
+export default Home;

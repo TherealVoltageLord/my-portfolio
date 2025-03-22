52b5e0e0b79a2ccb73c3353b019b0dc0faa6bfcc
VOLTAGE_LORD <officialvoltinstasnap@gmail.com>
2025-03-22 06:58:31 +0100

    Update SkillBar.jsx

--- src/components/SkillBar.jsx
+++ src/components/SkillBar.jsx
@@ -1,1 +1,12 @@
+const SkillBar = ({ skill, percentage }) => {
+  return (
+    <div className="skill">
+      <p>{skill}</p>
+      <div className="skill-bar">
+        <div className="skill-fill" style={{ width: `${percentage}%` }}></div>
+      </div>
+    </div>
+  );
+};
 
+export default SkillBar;

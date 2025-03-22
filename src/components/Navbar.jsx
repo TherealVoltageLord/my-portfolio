12a36dc04c7f8edff5c2ae9d9303d481a3a0b4ea
VOLTAGE_LORD <officialvoltinstasnap@gmail.com>
2025-03-22 07:02:06 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,2 +1,18 @@
-import { link } from
-  "react-router-dom";
+import { Link } from "react-router-dom";
+import { FaHome, FaUser, FaProjectDiagram, FaEnvelope } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Voltage Lord</h1>
+      <ul>
+        <li><Link to="/"><FaHome /> Home</Link></li>
+        <li><Link to="/about"><FaUser /> About</Link></li>
+        <li><Link to="/projects"><FaProjectDiagram /> Projects</Link></li>
+        <li><Link to="/contact"><FaEnvelope /> Contact</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
